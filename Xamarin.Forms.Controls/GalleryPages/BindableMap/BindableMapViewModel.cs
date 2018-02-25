167b8fbb156b5fd23f1801769c63e7cd38387dae
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-02-25 17:54:07 +0100

    Updated PinsSource Map Sample

--- Xamarin.Forms.Controls/GalleryPages/BindableMap/BindableMapViewModel.cs
+++ Xamarin.Forms.Controls/GalleryPages/BindableMap/BindableMapViewModel.cs
@@ -24,28 +24,31 @@ namespace Xamarin.Forms.Controls.GalleryPages.BindableMap
 
 		private void LoadPins()
 		{
-			Pins = new ObservableCollection<Pin>
+			Pins = new ObservableCollection<Pin>();
+
+			Pins.Add(new Pin
 			{
-				new Pin
-				{
-					Type = PinType.Place,
-					Position = new Position (41.890202, 12.492049),
-					Label = "Colosseum",
-					Address = "Piazza del Colosseo, 00184 Rome, Province of Rome, Italy"
-				},
-					new Pin {
-					Type = PinType.Place,
-					Position = new Position (41.898652, 12.476831),
-					Label = "Pantheon",
-					Address = "Piazza della Rotunda, 00186 Rome, Province of Rome, Italy"
-				},
-					new Pin {
-					Type = PinType.Place,
-					Position = new Position (41.903209, 12.454545),
-					Label = "Sistine Chapel",
-					Address = "Piazza della Rotunda, 00186 Rome, Province of Rome, Italy"
-				}
-			};
+				Type = PinType.Place,
+				Position = new Position(41.890202, 12.492049),
+				Label = "Colosseum",
+				Address = "Piazza del Colosseo, 00184 Rome, Province of Rome, Italy"
+			});
+
+			Pins.Add(new Pin
+			{
+				Type = PinType.Place,
+				Position = new Position(41.898652, 12.476831),
+				Label = "Pantheon",
+				Address = "Piazza della Rotunda, 00186 Rome, Province of Rome, Italy"
+			});
+
+			Pins.Add(new Pin
+			{
+				Type = PinType.Place,
+				Position = new Position(41.903209, 12.454545),
+				Label = "Sistine Chapel",
+				Address = "Piazza della Rotunda, 00186 Rome, Province of Rome, Italy"
+			});
 		}
 	}
 }
